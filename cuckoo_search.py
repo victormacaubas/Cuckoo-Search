17a21bb8b7e4ff71e182969526c822fa8bf2dfb8
Victor Macaubas <victor@macaubas.com>
2024-04-22 20:21:06 -0300

    modifying cuckoo class

--- cuckoo_search.py
+++ cuckoo_search.py
@@ -1,19 +1,15 @@
 import numpy as np
 import math
 
-
 class CuckooSearch:
     def __init__(self, problem, population_size, max_iterations):
-        self.problem = problem
+        self.problem = problem  # This is now an instance of an Optimization subclass
         self.population_size = population_size
         self.max_iterations = max_iterations
         self.alpha = 1.0  # Scaling factor for Lévy flights
         self.pa = 0.25  # Probability of discovering an alien egg
-        self.nests = []
-        self.fitness = []
 
     def levy_flight(self, beta=1.5):
-        """ Generate step sizes via Lévy flight. """
         sigma = (math.gamma(1 + beta) * math.sin(math.pi * beta / 2) /
                  (math.gamma((1 + beta) / 2) * beta * 2 ** ((beta - 1) / 2))) ** (1 / beta)
         u = np.random.normal(0, sigma, 1)
@@ -22,10 +18,9 @@ class CuckooSearch:
         return step
 
     def initialize_population(self):
-        self.nests = np.random.uniform(self.problem['bounds'][0], self.problem['bounds'][1],
-                                       (self.population_size, len(self.problem['variables'])))
-        self.fitness = np.array([self.problem['function'](nest)
-                                for nest in self.nests])
+        self.nests = np.random.uniform(self.problem.lower_bound, self.problem.upper_bound, 
+                                       (self.population_size, self.problem.dimension))
+        self.fitness = np.array([self.problem.test_function(nest) for nest in self.nests])
 
     def get_best_nest(self):
         best_idx = np.argmin(self.fitness)
@@ -35,20 +30,20 @@ class CuckooSearch:
         n_abandon = int(self.pa * self.population_size)
         worse_indices = np.argsort(self.fitness)[-n_abandon:]
         for idx in worse_indices:
-            self.nests[idx] = np.random.uniform(self.problem['bounds'][0], self.problem['bounds'][1],
-                                                (len(self.problem['variables'])))
-            self.fitness[idx] = self.problem['function'](self.nests[idx])
+            self.nests[idx] = np.random.uniform(self.problem.lower_bound, self.problem.upper_bound, 
+                                                (self.problem.dimension,))
+            self.fitness[idx] = self.problem.test_function(self.nests[idx])
 
     def run(self):
         self.initialize_population()
         best_nest, best_fitness = self.get_best_nest()
 
-        for iteration in range(self.max_iterations):
+        for _ in range(self.max_iterations):
             for i in range(self.population_size):
                 new_nest = self.nests[i] + self.alpha * self.levy_flight()
-                new_fitness = self.problem['function'](new_nest)
-
-                # Randomly choose a nest to replace (but not the best one)
+                new_nest = np.clip(new_nest, self.problem.lower_bound, self.problem.upper_bound)
+                new_fitness = self.problem.test_function(new_nest)
+                
                 j = np.random.randint(0, self.population_size)
                 if new_fitness < self.fitness[j]:
                     self.nests[j] = new_nest
@@ -57,13 +52,6 @@ class CuckooSearch:
                         best_nest = new_nest
                         best_fitness = new_fitness
 
-            # Abandon a fraction of the worst nests
             self.abandon_worse_nests()
-
-            # Find the best nest after potential changes
-            current_best, current_fitness = self.get_best_nest()
-            if current_fitness < best_fitness:
-                best_nest = current_best
-                best_fitness = current_fitness
 
         return best_nest, best_fitness
